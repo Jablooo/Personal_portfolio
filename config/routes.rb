7865fd2c749ab23337d6c279d17daeb59ba597dd
Jon Ablondi <jonnyablondi@hotmail.com>
2017-09-07 11:36:31 +1000

    portfolio routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'portfolio/project1'
-
-  get 'portfolio/project2'
 
   root 'pages#homepage'
 
+  # Contact links
   get '/contact', to: 'pages#contact'
+
+  # Portfolio links
+  get '/project1', to: 'portfolio#project1'
+  get '/project2', to: 'portfolio#project2'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
